bf27fc263684163a66e0a9301cfda9e0ab8dfa2d
Johan <johan@example.com>
2025-10-15 19:49:36 -0500

    Initial commit: Flutter autocar app

---
+++ README.md
@@ -0,0 +1,3 @@
+# autocar_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/autocar_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.autocar_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
